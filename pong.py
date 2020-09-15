79e6e8b7f9ca5f73650333f510610a7e889fff15
Mike <63983972+Teamwolf11@users.noreply.github.com>
2020-09-15 18:30:48 +1200

    create pong game
    I haven't done anything yet. just imported turtle and making the screen

---
+++ pong.py
@@ -0,0 +1,35 @@
+#simple pong game by Mike
+# by Mike 
+# part 1
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Mike Cui")
+wn.bgcolor("black")
+wn.setup(width=800,height=600  )
+wn.tracer(0)
+
+
+
+# paddle a
+paddleA=
+
+
+#paddle b
+
+
+
+
+
+
+
+
+#main game loop
+while True:
+  wn.update()
+
+
+
+
+
