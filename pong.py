3f99d6fcdf89a04be04b328cf9a0cc37d41b0e9f
teamwolf11 <mikecui5@gmail.com>
2020-11-03 21:38:47 +1300

    adding paddle and ball to gui

--- pong.py
+++ pong.py
@@ -15,16 +15,42 @@ window.tracer(0)
 
 
 # paddle a
-paddleA=
+paddleA= turtle.Turtle()
+paddleA.speed(0)#speed of animation is set to max
+paddleA.shape("square")
+paddleA.color("white")
+paddleA.shapesize(stretch_wid=5, stretch_len=1)
+paddleA.penup()
+paddleA.goto(-350, 0)
 
-#hi
+
 #paddle b
+paddleB= turtle.Turtle()
+paddleB.speed(0)#speed of animation is set to max
+paddleB.shape("square")
+paddleB.color("white")
+paddleB.shapesize(stretch_wid=5, stretch_len=1)
+paddleB.penup()
+paddleB.goto(350, 0)
+
+
+#Ball
+ball= turtle.Turtle()
+ball.speed(0)#speed of animation is set to max
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Function
+def paddle_a_up():
+
 
 
 
 #main game loop
 while True:
-  wn.update()
+  window.update()
 
 
 
